f4a123fcafff1a2acce1f084d32b088569f90591
Arif159357 <55071900+Arif159357@users.noreply.github.com>
2020-05-01 13:49:49 +0600

    Add files via upload

---
+++ googleSpreadsheet.js
@@ -0,0 +1,10 @@
+// $("#subbtn").click(function(e){
+//     window.location.href = "https://docs.google.com/spreadsheets/d/1YPRh0SZzl9pHKf4EX43nD1CyPCSrUtKd5mewTCJHJLA/edit#gid=0";
+// }) 
+    
+function gotosheet(event){
+
+    console.log("Im in")
+    // window.location.href = "https://docs.google.com/spreadsheets/d/1YPRh0SZzl9pHKf4EX43nD1CyPCSrUtKd5mewTCJHJLA/edit#gid=0";
+    window.location.replace("https://docs.google.com/spreadsheets/d/1YPRh0SZzl9pHKf4EX43nD1CyPCSrUtKd5mewTCJHJLA/edit#gid=0");
+}
